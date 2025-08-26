58a67b404df346c54ad1c01e4becb028799f80af
Shet Sayeesh Shrikanth (SX/ETE13-MM) <tww2kor@bosch.com>
2025-08-26 10:26:03 +0530

    updated monitor.cpp and test monitor

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,11 +1,8 @@
+
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(99, 102, 70));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
-}
-TEST(Monitor, NotOkWhenAnyVitalIsWithiNRange) {
-  ASSERT_FALSE(vitalsOk(99, 100, 70));
-  ASSERT_TRUE(vitalsOk(98, 70, 98));
 }
