b7600c359548df45315ec0ca0c8c94900bb09a03
Shet Sayeesh Shrikanth (SX/ETE13-MM) <tww2kor@bosch.com>
2025-08-26 10:19:57 +0530

    Test monitor changes

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -5,3 +5,7 @@ TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(99, 102, 70));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
 }
+TEST(Monitor, NotOkWhenAnyVitalIsWithiNRange) {
+  ASSERT_FALSE(vitalsOk(99, 100, 70));
+  ASSERT_TRUE(vitalsOk(98, 70, 98));
+}
